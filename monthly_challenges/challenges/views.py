23da281b44bbbf00f1ca3062903717fd587f43d0
Ancelli <xnazar507@gmail.com>
2025-01-05 17:08:04 +0200

    Added templates

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import render
+
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
@@ -42,7 +44,9 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month.lower()]
-        responce_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(responce_data)
+        return render(request, "challenges/challenge.html", {
+            "challenge_text": challenge_text,
+            "month": month.capitalize()
+        })
     except:
         return HttpResponseNotFound("<h1>Page not found</h1>")
